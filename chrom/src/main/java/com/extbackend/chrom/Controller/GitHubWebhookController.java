bbe086a2d64e51e3d58c2fc8e2e6ff8c3cf31cd5
VedantBorude683 <vedantBorude77@gmail.com>
2026-07-07 22:44:17 +0530

    done with automate entryyyy

--- chrom/src/main/java/com/extbackend/chrom/Controller/GitHubWebhookController.java
+++ chrom/src/main/java/com/extbackend/chrom/Controller/GitHubWebhookController.java
@@ -6,13 +6,13 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
-@RequestMapping("/api/webhooks")
+
 public class GitHubWebhookController {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    @PostMapping("/github")
+    @PostMapping("/github-webhook-receiver")
     public ResponseEntity<String> handleGitHubWebhook(@RequestBody String payload) {
         // 1. Send the raw GitHub PR payload to Kafka
         kafkaTemplate.send("pr-analysis-requests", payload);
